25a46620875950a6cb0540c01da7ec2a56846cbc
Maximiliano Zapata <maximilianozapata84@gmail.com>
2022-12-01 19:56:46 -0300

    pago

--- pagos.js
+++ pagos.js
@@ -1,4 +1,10 @@
 const pagar = document.getElementById("pagar");
 pagar.onclick = () => {    
-    Swal.fire('Compra finalizada con exito')
+    Swal.fire('Compra finalizada con exito');
+    carrito = []; 
+    carrito = localStorage.clear();    
+    guardaStorage();      
+    pintarCarrito();
+    actualizarCarrito();        
+    contarCarrito();     
 };
